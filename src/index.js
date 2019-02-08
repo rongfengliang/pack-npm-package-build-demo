9f3fddfbb84238f154c394e908a915e90ebb3987
dalongrong <1141591465@qq.com>
2019-02-08 15:59:39 +0800

    add bin demo

--- src/index.js
+++ src/index.js
@@ -12,5 +12,19 @@ var user = {
     name: "dalongdemo",
     age:333
 }
-export {PaltformLogin,user}
+export {PaltformLogin,user,run}
 export default user
+
+function cliapp(path,args){
+   
+     this.exefilepath= path
+     this.appname =args[0]
+}
+
+cliapp.prototype.catch=function(err) {
+    console.log(`some wrong hapends ${err}`)
+}
+function run(args) {
+     console.log(`this is a demo application ${JSON.stringify(user)},${JSON.stringify(args)}`)
+     return new cliapp(args[0],args[1],args[2])
+}
